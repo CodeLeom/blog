1d692346de09e2809aa484e9c39b0706b77dd88d
Ayodele Aransiola <36636824+CodeLeom@users.noreply.github.com>
2023-06-02 02:17:44 +0100

    styled the form wit tailwind

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,8 @@ import PostCreate from "./PostCreate";
 
 function App() {
   return (
-    <div>
-        <h1>Create Post</h1>
+    <div className="w-full max-w-xs">
+        <h1 className="text-gray-700 text-sm font-bold text-center">Create Post</h1>
         <PostCreate />
     </div>
   );
--- client/src/PostCreate.js
+++ client/src/PostCreate.js
@@ -2,15 +2,15 @@ import React from 'react'
 
 function PostCreate() {
   return (
-    <div>
-      <form>
-        <div className='form-group'>
-            <label>
+    <div className='w-full max-w-xs'>
+      <form className='bg-white shadow-md rounded px-8 pt--6 pb-8 mb-4'>
+        <div className='mb-4'>
+            <label className='block text-gray-700 text-sm font-bold mb-2' for='title'>
                 Title
             </label>
-            <input className='form-control' />
+            <input className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' />
         </div>
-        <button className='btn btn-primary'>Submit</button>
+        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline' type='button'>Submit</button>
       </form>
     </div>
   )

5b51eca17f2ddcde29b39e0780d14f0c6284baf3
Ayodele Aransiola <36636824+CodeLeom@users.noreply.github.com>
2023-06-06 22:56:07 +0100

    make a connection to the post service using axios

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import PostCreate from "./PostCreate";
 
 function App() {
   return (
-    <div className="w-full max-w-xs">
+    <div className="w-full max-w-xs my-9">
         <h1 className="text-gray-700 text-sm font-bold text-center">Create Post</h1>
         <PostCreate />
     </div>

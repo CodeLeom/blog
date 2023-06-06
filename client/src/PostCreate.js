11806b2f1bb2b5a96426b2421764f957ab608c0a
Ayodele Aransiola <36636824+CodeLeom@users.noreply.github.com>
2023-06-06 23:17:32 +0100

    modify html

--- client/src/PostCreate.js
+++ client/src/PostCreate.js
@@ -17,7 +17,7 @@ function PostCreate() {
     <div className='w-full max-w-xs mx-9'>
       <form onSubmit={onSubmit} className='bg-white shadow-md rounded px-8 pt--6 pb-8 mb-4'>
         <div className='mb-4'>
-            <label className='block text-gray-700 text-sm font-bold mb-2' for='title'>
+            <label className='block text-gray-700 text-sm font-bold mb-2'>
                 Title
             </label>
             <input value={title} onChange={e => setTitle(e.target.value)} className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' />

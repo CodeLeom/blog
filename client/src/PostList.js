7244083c470fe88a1572a13afe0152924aad0224
Ayodele Aransiola <36636824+CodeLeom@users.noreply.github.com>
2023-06-06 23:51:02 +0100

    display the posts

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -3,11 +3,33 @@ import axios from 'axios'
 
 function PostList() {
 
-    const [posts, setPosts] = useState([])
-    
+    const [posts, setPosts] = useState({})
+
+    //fetch the available posts from the Post endpoint
+    const fetchPosts = async () => {
+        const res = await axios.get('http://localhost:4001/posts')
+
+        setPosts(res.data)
+    }
+
+    //make the request from the Post once
+    useEffect(() => {
+        fetchPosts()
+    }, [])
+
+    const renderedPosts = Object.values(posts).map(post => {
+        return (
+            <div className='card' key={post.id}>
+                <div className='card-body'>
+                    <h3> {post.title} </h3>
+                </div>
+            </div>
+        )
+    })
+
   return (
     <div>
-      
+      {renderedPosts}
     </div>
   )
 }

0d20c0d643c9ac125be480de7c058246e427b40f
Ayodele Aransiola <36636824+CodeLeom@users.noreply.github.com>
2023-06-06 23:24:39 +0100

    modify html

--- client/src/PostCreate.js
+++ client/src/PostCreate.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 
 function PostCreate() {
@@ -22,7 +22,7 @@ function PostCreate() {
             </label>
             <input value={title} onChange={e => setTitle(e.target.value)} className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline' />
         </div>
-        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline' type='button'>Submit</button>
+        <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'>Submit</button>
       </form>
     </div>
   )

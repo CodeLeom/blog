99e4e97e62caad6b274cd0353e6fc2fad2b5b124
Ayodele Aransiola <36636824+CodeLeom@users.noreply.github.com>
2023-06-06 23:34:28 +0100

    added the post list component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import PostCreate from "./PostCreate";
+import PostList from "./PostList";
 
 function App() {
   return (
     <div className="w-full max-w-xs my-9">
         <h1 className="text-gray-700 text-sm font-bold text-center">Create Post</h1>
         <PostCreate />
+        <hr />
+        <h2>Posts</h2>
+        <PostList />
     </div>
   );
 }

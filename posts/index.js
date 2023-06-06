1282ef25d9209d73d1c9e62419d23222a4da68d4
Ayodele Aransiola <36636824+CodeLeom@users.noreply.github.com>
2023-06-06 23:18:33 +0100

    added cors as a middleware

--- posts/index.js
+++ posts/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const { randomBytes } = require('crypto')
+const cors = require('cors')
 
 const app = express()
 app.use(bodyParser.json())
+app.use(cors())
 
 // store posts in the memory (every post created get deleted anytime the app restarts)
 const posts = {}

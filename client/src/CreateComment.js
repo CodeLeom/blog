1c3600611a2f4351a7baf351501812606a296804
Ayodele Aransiola <36636824+CodeLeom@users.noreply.github.com>
2023-06-16 14:55:02 +0100

    api route changed for comment 'post'

--- client/src/CreateComment.js
+++ client/src/CreateComment.js
@@ -7,7 +7,7 @@ function CreateComment({postId}) {
     const onSubmit = async (e) => {
         e.preventDefault()
 
-        await axios.post(`http://localhost:4001/posts/${postId}/comments`,{
+        await axios.post(`http://localhost:4002/posts/${postId}/comments`, {
             content
         })
 

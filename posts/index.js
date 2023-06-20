a2cd2320946fca4a9223f9c0dadda97ada859309
Ayodele Aransiola <36636824+CodeLeom@users.noreply.github.com>
2023-06-20 16:35:28 +0100

    setup event emit on post service

--- posts/index.js
+++ posts/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const { randomBytes } = require('crypto')
 const cors = require('cors')
+const axios = require('axios')
 
 const app = express()
 app.use(bodyParser.json())
@@ -16,13 +17,21 @@ app.get('/posts', (req, res) => {
 })
 
 // make POST request to the endpoint
-app.post('/posts', (req, res) => {
+app.post('/posts', async (req, res) => {
     const id = randomBytes(4).toString('hex') //generate unique id for each post
     const { title } = req.body
 
     posts[id] = {
         id, title
     }
+
+    await axios.post("http://localhost:4005/events", {
+    type: "PostCreated",
+    data: {
+      id,
+      title,
+    },
+  })
 
     res.status(201).send(posts[id])
 })

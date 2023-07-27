0bc245f202965f02f294042aafde56430a2f3d86
Ayodele Aransiola <36636824+CodeLeom@users.noreply.github.com>
2023-07-27 20:08:58 +0100

    handler for all event coming in

--- comments/index.js
+++ comments/index.js
@@ -39,9 +39,21 @@ app.post('/posts/:id/comments', async (req, res) => {
     res.status(201).send(comments)
 })
 
-
+// POST request handler, that handles all incoming events
 app.post('/events', (req, res) => {
     console.log('Event Received', req.body.type)
+
+    const {type, data} = req.body
+    if(type === 'CommentModerated') {
+        const { postId, id, status } = data
+        
+        const comments = commentsByPostId[postId]
+
+        const comment = comments.find(comment => {
+            return comment.id === id
+        })
+        comment.status = status
+    }
 
     res.send({})
 })

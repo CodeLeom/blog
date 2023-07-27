195e145629334b362dc826301ebe8cf6b82a9926
Ayodele Aransiola <36636824+CodeLeom@users.noreply.github.com>
2023-07-27 23:34:35 +0100

    feat: implemented comment status to frontend

--- client/src/ListComment.js
+++ client/src/ListComment.js
@@ -5,7 +5,18 @@ function ListComment({ comments }) {
    //as we are now getting request directly from the query service
 
     const renderedComments = comments.map(comment => {
-        return <li key={comment.id}>{comment.content}</li>
+
+      let content
+
+      if(comment.status === 'approved'){
+        content = comment.content
+      } else if (comment.status === 'pending'){
+        content = 'This comment is awaiting moderation'
+      } else {
+        content = 'This comment has been rejected'
+      }
+
+        return <li key={comment.id}>{content}</li>
     })
   return (
     <ul className='list-disc px-3'>

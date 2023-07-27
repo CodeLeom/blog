0be50cae38207e942a697407068c049738588d94
Ayodele Aransiola <36636824+CodeLeom@users.noreply.github.com>
2023-07-27 23:57:28 +0100

    added a get request to retrieve every events

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,16 +5,24 @@ const axios = require('axios')
 const app = express()
 app.use(bodyParser.json())
 
+const events = []
+
 app.post('/events', (req, res) => {
     const event = req.body
+
+    events.push(event)
 
     axios.post('http://localhost:4001/events', event) //posts service
     axios.post('http://localhost:4002/events', event) //comments service
     axios.post('http://localhost:4003/events', event) //query service
     axios.post('http://localhost:4004/events', event) //moderation service
 
-
     res.send({ status: "OK"})
+})
+
+// retrieve every events that has occured
+app.get('/events', (req, res) => {
+    res.send(events)
 })
 
 app.listen(4005, () => {
